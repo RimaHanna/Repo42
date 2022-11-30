146c32127376e6af24fac26d564a016df2d1fd76
Rima Hanna <106812019+RimaHanna@users.noreply.github.com>
2022-11-30 09:41:19 +0100

    Add files via upload

---
+++ libft/ft_itoa.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhanna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 10:07:28 by rhanna            #+#    #+#             */
+/*   Updated: 2022/11/29 15:06:42 by rhanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+#include <limits.h>
+
+int	ft_nb_of_digit(int n)
+{
+	int	i;
+
+	i = 0;
+	while (n / 10 != 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	i++;
+	return (i);
+}
+
+char	*ft_nb_to_str(int n, int negative, int size, int div)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (size + negative + 1));
+	if (!str)
+		return (NULL);
+	if (negative)
+		str[i] = '-';
+	i += negative;
+	while (div / 10 != 0)
+	{
+		str[i] = (n / div) + '0';
+		n = n % div;
+		div = div / 10;
+		i++;
+	}
+	str[i] = n + '0';
+	i++;
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	int		size;
+	int		div;
+	int		negative;
+	char	*str;
+
+	negative = n < 0;
+	div = 1;
+	if (n == -2147483648)
+	{
+		str = (char *)malloc(sizeof(char) * 12);
+		if (!str)
+			return (NULL);
+		ft_strlcpy(str, "-2147483648", 12);
+		return (str);
+	}
+	if (negative)
+		n = -n;
+	size = ft_nb_of_digit(n);
+	while (size-- > 1)
+		div = div * 10;
+	return (ft_nb_to_str(n, negative, ft_nb_of_digit(n), div));
+}
+/*
+int main()
+{
+	int n = INT_MAX;
+	char *ret = ft_itoa(n);
+	printf("string: %s\n", ret);
+	return 0;
+}
+*/
---
+++ libft/ft_strtrim.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhanna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 10:34:20 by rhanna            #+#    #+#             */
+/*   Updated: 2022/11/24 10:34:48 by rhanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+#include <stdlib.h>
+
+/*la 1ere fonction va voir si les caracters de s1 existe dans set
+elle renvoie 1 si les caracteres existent*/
+
+/* l:  on est en train de voir si les caracteres existent au debut de 
+// la string et les depasser (donc on aura le nombre de caractere
+// de la debut de la chaine)*/
+
+/* end > start c a d que il ya de nombre de caractere suffisant dans
+// la chaine && on est en train de detecter les caractere set a la fin 
+// de la chaine, "end" est par exemple 10 elle va arreter apres 2 caracteres
+// donc a 8 */
+int	ft_find_char_in_set(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*str;
+
+	start = 0;
+	while (s1[start] && ft_find_char_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_find_char_in_set(s1[end - 1], set))
+		end--;
+	str = ft_substr(s1, start, end - start);
+	return (str);
+}
+/*
+int main()
+{
+	char s1[] = "abcdba";
+	char s2[] = "acb";
+	char *ret = ft_strtrim(s1, s2);
+	printf("%s\n", ret);
+	return 0;
+}*/
