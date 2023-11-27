2f142db5f952ea5bbb3656fa8736554fc0ed911f
rima <rima_hanna@live.com>
2023-11-27 15:45:16 +0100

    send

---
+++ C++/C++Module01/ex06/main.cpp
@@ -0,0 +1,50 @@
+#include "Harl.hpp"
+
+int	level_is(char *argv)
+{
+	int i = 0;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	while (i < 4)
+	{
+		if (levels[i] == argv)
+			return i;
+		i++;
+	}
+	return (-1);
+}
+
+void	harl_switch(char *argv, Harl & H)
+{
+	switch (level_is(argv))
+	{
+		case 0:
+			H.complain("DEBUG");
+			break;
+		case 1:
+			H.complain("INFO");
+			break;
+		case 2:
+			H.complain("WARNING");
+			break;
+		case 3:
+			H.complain("ERROR");
+			break;
+		default:
+			std::cout << "Probably complaining about insignificant problems" << std::endl;
+			break;
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	Harl H;
+	if (argc != 2)
+	{
+		std::cout << "Harl i don't want to hear everything!!!" << std::endl;
+		return (-1);
+	}
+	else
+		harl_switch(argv[1], H);
+	return (0);
+}
