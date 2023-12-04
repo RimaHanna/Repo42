bfabd35d1131881373cf83674300f9dfe440d1b4
rima <rima_hanna@live.com>
2023-12-04 16:33:47 +0100

    send

--- C++/C++Module01/ex05/Harl.cpp
+++ C++/C++Module01/ex05/Harl.cpp
@@ -12,7 +12,6 @@ Harl::~Harl()
 
 void	Harl::debug(void)
 {
-	std::cout << "DEBUG		";
 	std:: cout << "I love having extra bacon for my";
 	std::cout << "7XL-double-cheese-triple-pickle-specialketchup ";
 	std::cout << "burger. I really do!" << std::endl;
@@ -20,26 +19,22 @@ void	Harl::debug(void)
 
 void	Harl::info(void)
 {
-	std::cout << "INFO		";
 	std::cout << "I cannot believe adding extra bacon costs more money. ";
 	std::cout << "You didn’t put enough bacon in my burger! ";
-	std::cout << "If you did, I" << std::endl;
-	std::cout << "			wouldn't be asking for more!" << std::endl;
+	std::cout << "If you did, I wouldn't be asking for more! " << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "WARNING		";
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve ";
-	std::cout << "been coming for years whereas you started working here" << std::endl;
-	std::cout << "			since last month."<< std::endl;
+	std::cout << "been coming for years whereas you started working here";
+	std::cout << "since last month."<< std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "ERROR		";
 	std::cout << "This is unacceptable! ";
-	std::cout << " I want to speak to the manager now." << std::endl;
+	std::cout << "I want to speak to the manager now." << std::endl;
 }
 
 void	Harl::complain(std::string level)
--- C++/C++Module01/ex05/main.cpp
+++ C++/C++Module01/ex05/main.cpp
@@ -3,10 +3,22 @@
 int main()
 {
 	Harl H;
-	
-	H.complain("DEBUG");
-	H.complain("INFO");
-	H.complain("WARNING");
-	H.complain("ERROR");
+	std::string level;
+
+	while(1)
+	{
+		std::cout << "Enter a valid level for Harl: "; 
+		if (!std::getline(std::cin, level))
+		{
+			std::cout << std::endl;
+			std::cout << "Ctrl+D detected. End of input." << std::endl;
+			return (0);
+		}
+		if (!level.empty() && (level == "DEBUG" || level == "INFO" 
+				|| level == "WARNING" || level == "ERROR"))
+		{
+			H.complain(level);
+		}
+	}
 	return (0);
 }
