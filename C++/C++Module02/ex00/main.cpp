3cfafdd44bdc64904dd84e2f44a939bed290c386
rima <rima_hanna@live.com>
2023-12-14 12:45:03 +0100

    slight modification

--- C++/C++Module02/ex00/main.cpp
+++ C++/C++Module02/ex00/main.cpp
@@ -19,3 +19,25 @@ int main( void )
     std::cout << c.getRawBits() << std::endl;
     return 0;
 }
+
+/*
+// OUTPUT SHOULD BE
+$> ./a.out
+Default constructor called
+Copy constructor called
+Copy assignment operator called // <-- This line may be missing depending on your implementation
+getRawBits member function called
+Default constructor called
+Copy assignment operator called
+getRawBits member function called
+getRawBits member function called
+0
+getRawBits member function called
+0
+getRawBits member function called
+0
+Destructor called
+Destructor called
+Destructor called
+$>
+*/
