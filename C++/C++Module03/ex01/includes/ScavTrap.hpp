f1d142c05ab3592d54565c3a71c4ba787c92aadd
rima <rima_hanna@live.com>
2023-12-27 15:21:04 +0100

    send

--- C++/C++Module03/ex01/includes/ScavTrap.hpp
+++ C++/C++Module03/ex01/includes/ScavTrap.hpp
@@ -4,19 +4,26 @@
 #include <string>
 #include "ClapTrap.hpp"
 
+/* When inheriting from a base class to a derived class,
+ the derived class does not need to repeat the entire 
+ canonical form of the base class. The derived class 
+ inherits the constructors, destructor, 
+and assignment operators from the base class.
+ScavTrap (const ScavTrap &to_copy);
+ScavTrap &operator = (const ScavTrap &original); 
+*/
+
 class ScavTrap : public ClapTrap
 {
     public:
         ScavTrap(std::string name);
         ~ScavTrap(void);
-//        ScavTrap (const ScavTrap &to_copy);
-//        ScavTrap &operator = (const ScavTrap &original);
         void attack(const std::string& target);
         void guardGate();
 
     private:
-        // Private constructor serving in a way to prevent the
-        // creation of Scavtrap witthout a name;
+    /* Private constructor serving in a way to prevent the
+     creation of Scavtrap witthout a name; */
         ScavTrap(void);
 
 };
