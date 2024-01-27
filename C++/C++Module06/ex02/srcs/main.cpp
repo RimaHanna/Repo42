fa25e6a92cca65e8998a2c2118b82a33299e0da5
rima <rima_hanna@live.com>
2024-01-27 16:28:07 +0100

    send

--- C++/C++Module06/ex02/srcs/main.cpp
+++ C++/C++Module06/ex02/srcs/main.cpp
@@ -29,6 +29,25 @@ Base* generate(void)
     return NULL;
 }
 
+/**
+ * dynamic_cast is primarily used for downcasting, which means converting 
+ * a pointer or reference of a base class to a pointer or reference of a 
+ * derived class.
+ * 
+ * The classes involved in dynamic_cast must be polymorphic, i.e., 
+ * they must have at least one virtual function. This is because dynamic_cast 
+ * uses runtime type information (RTTI) to perform the cast.
+ * 
+ * Syntax:
+ *      dynamic_cast<DerivedType*>(BasePointer)
+ *      dynamic_cast<DerivedType&>(BaseReference)
+ * 
+ * Return Value:
+ *  If the cast is successful, it returns a pointer or reference to the derived type.
+ *  If the cast is not possible (e.g., the actual object is not of the derived type), 
+ * it returns a null pointer for pointers or throws a std::bad_cast exception for references.
+*/
+
 void identify(Base* p)
 {
     cout << "Attempting to identify type with *: " << endl;
