fe7d02094231523dc1a2f8c8ecb3507e5b546f1b
rima <rima_hanna@live.com>
2024-01-29 12:13:10 +0100

    send

---
+++ C++/C++Module07/ex01/includes/iter.hpp
@@ -0,0 +1,34 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <cstddef>
+#include <iostream>
+
+// The printElement function template takes an element of type T
+// and prints it to the standard output.
+// Parameter:
+//   - element: The element to be printed.
+template <typename T>
+void printElement(T &element)
+{
+    std::cout << element << " " << std::endl;
+}
+
+// The iter function template takes an array, its length, and a function,
+// and applies the provided function to each element of the array.
+// Parameters:
+//   - array: The address of an array.
+//   - length: The length of the array.
+//   - func: A function to be applied to each element of the array.
+template <typename T>
+void iter(T *array, size_t length, void (*func)(T &element))
+{
+    size_t i = 0;
+    while (i < length)
+    {
+        func(array[i]);
+        i++;
+    }
+}
+
+#endif
---
+++ C++/C++Module07/ex01/srcs/main.cpp
@@ -0,0 +1,38 @@
+#include "iter.hpp"
+#include <iostream>
+
+/**
+ * size_t intArrayLength = sizeof(intArray) / sizeof(int);
+ * This method calculates the total size of the array in bytes 
+ * (sizeof(myArray)) and divides it by the size of one element 
+ * (sizeof(myArray[0])) to get the number of elements.
+*/
+
+int main()
+{
+    // Test with an array of integers
+    int intArray[] = {1, 2, 3, 4, 5};
+    size_t intArrayLength = sizeof(intArray) / sizeof(int);
+
+    std::cout << "Iterating over intArray: " << std::endl;
+    ::iter(intArray, intArrayLength, &::printElement<int>);
+    std::cout << std::endl;
+
+    // Test with an array of doubles
+    double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    size_t doubleArrayLength = sizeof(doubleArray) / sizeof(double);
+
+    std::cout << "Iterating over doubleArray: " << std::endl;
+    iter(doubleArray, doubleArrayLength, ::printElement<double>);
+    std::cout << std::endl;
+
+    // Test with an array of strings
+    std::string stringArray[] = {"apple", "banana", "orange"};
+    size_t stringArrayLength = sizeof(stringArray) / sizeof(std::string);
+
+    std::cout << "Iterating over stringArray: " << std::endl;
+    ::iter(stringArray, stringArrayLength, printElement<std::string>);
+    std::cout << std::endl;
+
+    return 0;
+}
