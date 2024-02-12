aeb4369ac20a9ad507c177001de8eff3a91a6110
rima <rima_hanna@live.com>
2024-02-12 23:10:33 +0100

    send

--- C++/C++Module07/ex00/includes/whatever.hpp
+++ C++/C++Module07/ex00/includes/whatever.hpp
@@ -1,6 +1,16 @@
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+/**
+ * templates provide a way to create functions and classes that 
+ * work with any data type. They allow you to write generic code
+ * that can be used with different types without specifying the 
+ * type beforehand.
+ * The template <typename T> part is a declaration of a template parameter T.
+ * It tells the compiler that the following code is a template, 
+ * and T is a placeholder for a type that will be specified when the template is used.
+*/
+
 template <typename T>
 void swap(T &a, T &b)
 {
